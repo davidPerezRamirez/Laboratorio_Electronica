ef2ba66a68378dd6510378596796389273680c62
davidPerezRamirez <pucarauntref@gmail.com>
2016-10-13 21:53:31 -0300

    lectura de temperatura segun voltaje

--- PIC_DISPLAY_SEVEN_SEGMENTS/main.c
+++ PIC_DISPLAY_SEVEN_SEGMENTS/main.c
@@ -17,12 +17,10 @@
  * THIS SOFTWARE. THE COMPANY SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR
  * SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
  ******************************************************************************/
-
 #include <xc.h>
 #include <delays.h>
 #include <unmc_config_01.h>
 #include <unmc_inout_01.h>
-
 #include <unmc_display.h>
 
 void setup(void){  
@@ -34,17 +32,17 @@ void setup(void){
     OSCCONbits.IRCF2=1;
     OSCCONbits.SCS0=0;          //oscilator INTRC
     OSCCONbits.SCS1=0;
-    TRISA = 0b11110000;
+    TRISA = 0b11111000;
     TRISB = 0;
-    TRISC = 0b00100111;
+    TRISC = 0b00000111;
     //TRISAbits.TRISA0=1;
     //TRISBbits.TRISB0=0;
     //TRISCbits.TRISC0=0;
-    ANCON0=0b11111111;          // Config AN7 to AN0 Digital Ports
+    ANCON0=0b11111110;          // Config AN7 to AN0 Digital Ports
     //ANCON1=0b10010111;        // Config AN11 Analog Port
     ANCON1=0b11111111;          // Config AN12 to AN8 Digital Ports
-    ADCON0=0b00101101;          // Control AN11 Analog Port
-    ADCON1=0b00010000;          // Config Analog Port
+    ADCON0=0b01000000;          // Control AN11 Analog Port
+    ADCON1=0b00111000;          // Config Analog Port   
     RTCCFGbits.RTCEN=1;
     RTCCFGbits.RTCWREN=1;
     T1CONbits.T1OSCEN=1;
@@ -65,35 +63,48 @@ void imprimir_temperatura(int temp){
   
 }
 
+int calcular_temperatura(int voltage){
+    
+    return (int)(0.588 * voltage - 50);
+}
+
+int leer_temperatura(){
+    ADCON0bits.ADON = 1;
+    ADCON0bits.GO = 1;
+    int voltage;
+    
+    while (ADCON0bits.GO);
+    __delay_ms(1);
+    voltage = ADRESH;
+    ADCON0bits.ADON = 0;   
+    return calcular_temperatura(voltage);
+}
+
 int main(void){
     
     setup();
+    int temperatura = 0; 
+    int valor = 99;
+    int i;
     
-    int dec = 0;
-    int uni = 0;
-    int temperatura = 0;
-    int contador = 0;  
-    
-    while(1){                                               
-      
-      //el while evita parpadeo
-      while (contador <10 )  {
-        temperatura = (dec*10) + uni;
-        imprimir_temperatura(temperatura);
-        contador++;
-      }      
-      contador = 0;
-      //if (!switch1){
-           uni++;
-           if (uni > 9){               
-               dec++;
-               uni = 0;               
-           }
-           
-           if (dec > 9){
-                dec = 0;
-           }
-       //}           
+    while(1){  
+        
+        temperatura = leer_temperatura();
+        if (temperatura < valor){
+            valor--;
+        }else{
+            if (temperatura > valor){
+                valor++;
+            }
+            else
+                valor = temperatura;
+        }
+
+        i = 0;
+        while (i < 10){
+            imprimir_temperatura(valor);
+            i++;
+        }         
     }
     
     return 0;
